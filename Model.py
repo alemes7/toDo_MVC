5e039c7388c567e149e18b2cc3ca7c568f655797
Japiinhaa <vitorgoncalves964@gmail.com>
2023-10-05 16:00:24 -0300

    update

---
+++ Model.py
@@ -0,0 +1,12 @@
+class ToDo():
+    def __init__(self):
+        self.lista = []
+            
+    def AdicionarTarefa():
+        pass
+        
+    def RemoverTarefa():
+        pass
+        
+    def ListarTarefas():
+        pass
